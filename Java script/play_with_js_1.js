46decb767cbf4c16aa7811eac2e5d70ca89a91e8
Kiransura275 <udaykiransura143@gmail.com>
2025-02-10 23:51:55 +0530

    added js Fundamentals

---
+++ Java script/play_with_js_1.js
@@ -0,0 +1,112 @@
+//range sum functions
+
+// const arr = [ 1, 2 , 4 , 5 , 3] 
+// console.log(arr)
+// console. log( typeof arr)
+//Data types
+// let a = 10;
+// console.log(a,typeof a)
+// console.log(`${a}--->4${typeof a}`)
+
+// let b =20.3345
+// console.log(`${b}--->4${typeof b}`)
+
+// console.log(`${0.2+0.1}  ->`,0.3 ===0.1+0.2)
+
+// Arithmatic operators
+
+// let a = 3
+// console.log(typeof a);
+
+// let b = 1.5
+// // console.log(a+b,a-b,a*b,a/b,a%b,a**b, b+a,b-a,b*a,b/a,Math.ceil(b%a),Math.floor(b**a));
+// let a="10"
+// let b = "20.A"
+// console.log(typeof (a*b));
+// console.log(-1/0);
+// console.log(Math.sign(-1/0));
+// console.log(-1 === 1);
+// let a= 1e3;
+// console.log(a);
+
+// // strings 
+// let str = "i am uday kiran "
+// console.log(str);
+// console.log(str.length);
+// console.log(typeof str);
+// let a = "hi"
+// let b = "hib"
+// console.log(a,b);
+
+// console.log(a.length);
+// // console.log(a < b);
+// console.log(a+b, a-b , a*b , a/b);
+
+// Boolean 
+// let a = true;
+// let b = 1>2;
+// console.log(a , typeof a);
+// console.log(b , typeof b);
+// console.log(false == 0);
+// if(undefined)
+// {
+//     console.log
+//     ("hi")
+// }
+
+// console.log(Boolean(""));
+
+// logical operators
+// // && || !
+// console.log(2||3);
+// console.log(4 ||6)
+// console.log(!NaN);
+
+// Arithmatic operations with strings
+// let a= "10";
+// let b = undefined
+// console.log(a+b, a*b , a-b , a/b , a%b , a**b  );
+
+// let a;
+// console.log(a);
+// console.log(typeof a);
+// a = null;
+// console.log(a, typeof a);
+// const a = undefined;
+// a= null  //type Error
+// console.log(a, typeof a);
+
+// console.log(null == undefined);
+// console.log(NaN == undefined);
+
+// arr = [1,23 , 5]
+// console.log(arr.__proto__)
+// console.log(1/0 === Infinity);
+
+
+// // let if = 1;
+// // console.log(let)
+
+
+
+// console.log ( typeof console.log)
+// console.log(Math.max(1,2,3,4,5,6))
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
